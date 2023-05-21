b83d4542888ed7efb26ff417f79beb29ba03d623
minh-nguyenhoang <82560350+minh-nguyenhoang@users.noreply.github.com>
2023-05-21 15:31:00 +0700

    Update critic.py

--- spoof_gen/models/critic.py
+++ spoof_gen/models/critic.py
@@ -24,31 +24,46 @@ class Critic(nn.Module):
         #     params.requires_grad = False
 
         self.upsampler = nn.Upsample((input_dims[0],input_dims[1]),mode = 'bilinear')
-
-        self.critic = nn.Sequential(
-            Conv2d_cd(4, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        
+        self.aux_cls = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=1, stride=1, padding=0, bias=False),
+            # nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 1, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),)  #[16,16]
+        
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(4, 64, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
-            Conv2d_cd(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),         #[128,128,128]
-            Conv2d_cd(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            # nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),        #[256,64,64]
-            Conv2d_cd(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2), 
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),        #[512,32,32]
-            Conv2d_cd(512, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            # nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2), 
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),)        #[512,32,32]
+        self.critic = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            Conv2d_cd(128, 1, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(128, 1, kernel_size=3, stride=1, padding=1, bias=False),
             nn.LeakyReLU(0.2),
             nn.Flatten(),
             nn.Linear(32*32,1),
-            ScaledTanh(score_scale)
+#             ScaledTanh(score_scale)
         )
 
 
@@ -60,9 +75,11 @@ class Critic(nn.Module):
         # print(x.shape)
         input = torch.cat([x,map_x],dim = 1)
 
-        score = self.critic(input)
+        feaure = self.feature_extractor(input)
+        score = self.critic(feaure)
+        cls = self.aux_cls(feature)
 
-        return score
+        return score, cls
     
     def get_grad(self):
         grad = {}
